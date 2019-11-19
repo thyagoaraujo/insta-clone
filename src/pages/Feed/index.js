c84929668c1c9294f28b15afce62a89f9ce47bda
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-11-19 11:33:29 -0300

    config pagination

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -5,26 +5,35 @@ import { Post, Header, Avatar, Name, PostImage, Description } from './styles';
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  async function loadPage(pageNumber = page) {
+    if (total && pageNumber > total) return;
+
+    const response = await fetch(
+      `http://192.168.1.111:3333/feed?_expand=author&_limit=5&_page=${pageNumber}`
+    );
+
+    const data = await response.json();
+    const totalItems = response.headers.get('X-Total-Count');
+
+    setTotal(Math.floor(totalItems / 5));
+    setFeed([...feed, ...data]);
+    setPage(pageNumber + 1);
+  }
 
   useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        'http://192.168.1.111:3333/feed?_expand=author&_limit=5&_page=1'
-      );
-
-      const data = await response.json();
-
-      setFeed(data);
-    }
-
-    loadFeed();
-  }, []);
+    loadPage();
+  }, []); //eslint-disable-line
 
   return (
     <View>
       <FlatList
         data={feed}
         keyExtractor={post => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
         renderItem={({ item }) => (
           <Post>
             <Header>
