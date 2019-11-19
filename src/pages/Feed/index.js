854667eb495e48fc89ce486890e285eede2eb469
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-11-19 10:23:44 -0300

    get data Feed api

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, FlatList } from 'react-native';
 
-// import { Container } from './styles';
+import { Post, Header, Avatar, Name, PostImage, Description } from './styles';
 
 export default function Feed() {
-  return <View />;
+  const [feed, setFeed] = useState([]);
+
+  useEffect(() => {
+    async function loadFeed() {
+      const response = await fetch(
+        'http://192.168.1.111:3333/feed?_expand=author&_limit=5&_page=1'
+      );
+
+      const data = await response.json();
+
+      setFeed(data);
+    }
+
+    loadFeed();
+  }, []);
+
+  return (
+    <View>
+      <FlatList
+        data={feed}
+        keyExtractor={post => String(post.id)}
+        renderItem={({ item }) => (
+          <Post>
+            <Header>
+              <Avatar source={{ uri: item.author.avatar }} />
+              <Name>{item.author.name}</Name>
+            </Header>
+
+            <PostImage source={{ uri: item.image }} />
+
+            <Description>
+              <Name>{item.author.name}</Name> {item.description}
+            </Description>
+          </Post>
+        )}
+      />
+    </View>
+  );
 }
