32fbe4189bb72e0e91d9a35b45ee0ee166838aca
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-11-19 14:20:44 -0300

    add Animated

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -5,6 +5,7 @@ import { Small, Original } from './styles';
 const OriginalAnimated = Animated.createAnimatedComponent(Original);
 
 export default function LazyImage({ smallSource, source, aspectRatio }) {
+  const opacity = new Animated.Value(0);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
@@ -13,7 +14,13 @@ export default function LazyImage({ smallSource, source, aspectRatio }) {
     }, 1000);
   }, []);
 
-  function handleAnimate() {}
+  function handleAnimate() {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }
 
   return (
     <Small
@@ -24,6 +31,7 @@ export default function LazyImage({ smallSource, source, aspectRatio }) {
     >
       {loaded && (
         <OriginalAnimated
+          style={{ opacity }}
           source={source}
           ratio={aspectRatio}
           resizeMode="contain"
